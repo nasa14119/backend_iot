4832dbfb0287affad070afe843307d48f27b32f5
nasa14119 <nico.amaya0205@gmail.com>
2025-10-11 18:45:26 -0600

    db(schema): data schema adding cascading effect for deletio, also adding the sensors insetion type for the insertion data

--- gateway/src/db/schema.ts
+++ gateway/src/db/schema.ts
@@ -8,10 +8,11 @@ export const dataTable = sqliteTable("data", {
 export const sensorsTable = sqliteTable("sensors_data", {
   date: T.int({ mode: "timestamp_ms" })
     .primaryKey()
-    .references(() => dataTable.date),
+    .references(() => dataTable.date, { onDelete: "cascade" }),
   sensor_1: T.int({ mode: "boolean" }).default(false),
   sensor_2: T.int({ mode: "boolean" }).default(false),
   sensor_3: T.int({ mode: "boolean" }).default(false),
 });
-export type insert_data = typeof dataTable.$inferInsert;
+export type insert_data = typeof dataTable.$inferInsert &
+  typeof sensorsTable.$inferInsert;
 export type select_data = typeof dataTable.$inferSelect;
